23c9b776c8f34b27afcc961873b22e49bc101dea
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-04-06 00:35:49 -0500

    clase06

--- index.js
+++ index.js
@@ -5,18 +5,40 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
-})
+});
 
 app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy una nueva ruta');
-})
+});
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 3500
+    },
+    {
+      name: 'Product2',
+      price: 2000
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({id,
+    name: 'Product2',
+    price: 2000
+  })
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
   res.json({
-    name: 'Product 1',
-    price: 3500
-  });
+    categoryId,
+    productId,
+  })
 })
 
 app.listen(port, () => {
   console.log('Mi port ' + port)
-})
+});
