dd4d3d92813510e605364cf116631735214ecc82
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-04-07 11:53:02 -0500

    middleware de manejo de errores

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const { errorHandler, logErrors, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
 const port = 3000;
@@ -56,6 +57,10 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Mi port ' + port)
