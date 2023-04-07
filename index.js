67141bb63b91055c01fc0a29cedc210d22e93bbb
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-04-07 14:01:26 -0500

    final

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const whitelist = ['http://127.0.0.1:5500'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('No permitido'))
