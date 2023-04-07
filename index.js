556c95cf5694d1c2c5c413123d7096774b4be1a9
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-04-07 07:45:33 -0500

    modulizando

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
 const express = require('express');
-const app = express();
+const routerApi = require('./routes');
 
+const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
-
 app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy una nueva ruta');
 });
-app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 3500
-    },
-    {
-      name: 'Product2',
-      price: 2000
-    }
-  ]);
-});
 
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({id,
-    name: 'Product2',
-    price: 2000
-  })
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  })
-})
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Mi port ' + port)
